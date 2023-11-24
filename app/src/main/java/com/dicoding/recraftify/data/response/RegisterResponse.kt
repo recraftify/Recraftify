ec4e885d1eb14b5fbbfdcf7d7d1eaaa1bc39600a
Delinda Candrawati <delindap26@gmail.com>
2023-11-24 17:18:42 +0700

    tambah error response

--- app/src/main/java/com/dicoding/recraftify/data/response/RegisterResponse.kt
+++ app/src/main/java/com/dicoding/recraftify/data/response/RegisterResponse.kt
@@ -10,3 +10,11 @@ data class RegisterResponse(
 	@field:SerializedName("message")
 	val message: String
 )
+
+data class ErrorResponse(
+	@field:SerializedName("error")
+	val error: Boolean? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
