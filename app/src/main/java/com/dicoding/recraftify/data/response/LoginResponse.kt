7c843ced5d15a4738b5a5556400feea3b4261654
Delinda Candrawati <delindap26@gmail.com>
2023-12-07 12:27:20 +0700

    tambah login response

--- app/src/main/java/com/dicoding/recraftify/data/response/LoginResponse.kt
+++ app/src/main/java/com/dicoding/recraftify/data/response/LoginResponse.kt
@@ -3,25 +3,9 @@ package com.dicoding.recraftify.data.response
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-
-	@field:SerializedName("loginResult")
-	val loginResult: LoginResult,
-
-	@field:SerializedName("error")
-	val error: Boolean,
-
 	@field:SerializedName("message")
-	val message: String
-)
-
-data class LoginResult(
-
-	@field:SerializedName("name")
-	val name: String,
-
-	@field:SerializedName("userId")
-	val userId: String,
+	val message: String? = null,
 
 	@field:SerializedName("token")
-	val token: String
+	val token: String? = null
 )
